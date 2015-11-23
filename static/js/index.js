8c938bdd8d55e0a33281602f34e96c26bdbdb733
jiaoyilun <jiaoyilun@gmail.com>
2015-11-23 23:58:21 +0800

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -83,7 +83,7 @@ function categoryDisplay() {
     /*show category when click categories list*/
     $('.categories-list-item').click(function() {
         var cate = $(this).attr('cate'); //get category's name
-        $('#post-cate').replaceWith(cate);
+        $('#post-cate').html(cate);
         console.log("------"+cate);
         $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
         $('.post-list-body>div[post-cate=' + cate + ']').show(400);
