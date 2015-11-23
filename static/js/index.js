319cd0434c31a0f483f7139cb2f4e90b41149179
jiaoyilun <jiaoyilun@gmail.com>
2015-11-23 23:59:04 +0800

    commit

--- static/js/index.js
+++ static/js/index.js
@@ -84,7 +84,6 @@ function categoryDisplay() {
     $('.categories-list-item').click(function() {
         var cate = $(this).attr('cate'); //get category's name
         $('#post-cate').html(cate);
-        console.log("------"+cate);
         $('.post-list-body>div[post-cate!=' + cate + ']').hide(250);
         $('.post-list-body>div[post-cate=' + cate + ']').show(400);
     });
